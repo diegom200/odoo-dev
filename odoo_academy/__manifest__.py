8a37aed8fe47cfb831c33846ae3354ad6f865e8f
diegom200 <diego@m200la.com>
2022-09-27 18:22:37 +0000

    [ADD] rama-academy4

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,6 +15,7 @@
     'website': 'https://www.test.com',
     
     'category': 'Training',
+    'license': 'OPL-1',
     'version': '0.1',
     'depends': ['base'], 
     
@@ -23,6 +24,7 @@
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
+        'views/session_views.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
--- odoo_academy/models/__init__.py
+++ odoo_academy/models/__init__.py
@@ -1,1 +1,3 @@
 from . import course
+from . import session
+
