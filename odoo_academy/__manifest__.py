a63715c76b83cefbd2fed7cc494a33b95ba9d580
“diegom200” <diego@m200la.com>
2022-09-30 20:39:04 +0000

    [ADD]rama-academy7

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -17,7 +17,7 @@
     'category': 'Training',
     'license': 'OPL-1',
     'version': '0.1',
-    'depends': ['sale'], 
+    'depends': ['sale','website'], 
     
     'data': [
         'security/academy_security.xml',
@@ -29,6 +29,7 @@
         'views/product_views_inherit.xml',
         'wizard/sale_wizard_view.xml',
         'report/session_report_templates.xml',
+        'views/academy_web_templates.xml',
         
     ],
     'demo': [
